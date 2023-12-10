199c8850686902bb734f5dda16191e329df3497f
Alexander <coder27sinitsyn@gmail.com>
2023-12-10 21:06:28 +0300

    categories_mvc

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -13,6 +13,45 @@ class CategoriesController {
             res.status(500).json({message: e.message});
         }
     }
+
+    async create(req, res) {
+        const { title, url } = req.body
+        try {
+            if (title.length < 3 || title.length > 15)
+                throw new Error('Число символов меньше 3 или больше 15!')
+
+            if (!/^(http|https):\/\/([\w-]+(\.[\w-]+)+)(\/[\w-./?%&=]*)?$/.test(url))
+                throw new Error('Ссылка не валидна!')
+
+            const category = await categoriesService.create({title, url})
+            res.status(200).json(category)
+        } catch (e) {
+            res.status(500).json({message: e.message})
+        }
+    }
+
+    async deleteOne(req, res) {
+        const {id} = req.params
+
+        try {
+            const delCategory = await categoriesService.deleteOne(id)
+            res.status(200).json(delCategory)
+        } catch (e) {
+            res.status(500).json({message: e.message})
+        }
+    }
+
+    async update(req, res) {
+        const { id } = req.params
+        const { title, url } = req.body
+
+        try {
+            const upCategory = await categoriesService.update(id, { title, url })
+            res.status(200).json(upCategory)
+        } catch (e) {
+            res.status(500).json({message: e.message})
+        }
+    }
 }
 
 module.exports = new CategoriesController(categoriesService);
